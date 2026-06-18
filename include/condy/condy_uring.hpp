40d036de9088718632ca2f09af3f08f6defc9405
y1tang <yi1tang.yang@gmail.com>
2026-06-18 10:23:36 +0800

    polyfill io_uring_{setup,free}_buf_ring (#142)

--- include/condy/condy_uring.hpp
+++ include/condy/condy_uring.hpp
@@ -4,7 +4,11 @@
 
 #pragma once
 
-#include <liburing.h> // IWYU pragma: export
+#include <liburing.h>
+
+#include <cerrno>
+#include <cstring>
+#include <sys/mman.h>
 
 // liburing <= 2.3 has no version macros, define them here
 
@@ -33,5 +37,50 @@ inline void io_uring_prep_uring_cmd(struct io_uring_sqe *sqe, int cmd_op,
     sqe->__pad1 = 0;
     sqe->addr = 0ul;
     sqe->len = 0;
+}
+#endif
+
+// Polyfill for io_uring_setup_buf_ring / io_uring_free_buf_ring
+// (added in liburing 2.4)
+#if IO_URING_CHECK_VERSION(2, 4) // < 2.4
+inline struct io_uring_buf_ring *
+io_uring_setup_buf_ring(struct io_uring *ring, unsigned int nentries, int bgid,
+                        unsigned int flags, int *err) noexcept {
+    size_t ring_size = nentries * sizeof(struct io_uring_buf);
+    auto *br = static_cast<struct io_uring_buf_ring *>(
+        mmap(nullptr, ring_size, PROT_READ | PROT_WRITE,
+             MAP_ANONYMOUS | MAP_PRIVATE, -1, 0));
+    if (br == MAP_FAILED) {
+        *err = -errno;
+        return nullptr;
+    }
+
+    io_uring_buf_ring_init(br);
+
+    struct io_uring_buf_reg reg = {};
+    reg.ring_addr = reinterpret_cast<uint64_t>(br);
+    reg.ring_entries = nentries;
+    reg.bgid = bgid;
+
+    *err = 0;
+    int ret = io_uring_register_buf_ring(ring, &reg, flags);
+    if (ret != 0) {
+        munmap(br, ring_size);
+        *err = ret;
+        return nullptr;
+    }
+
+    return br;
+}
+
+inline int io_uring_free_buf_ring(struct io_uring *ring,
+                                  struct io_uring_buf_ring *br,
+                                  unsigned int nentries, int bgid) noexcept {
+    int ret = io_uring_unregister_buf_ring(ring, bgid);
+    if (ret != 0) {
+        return ret;
+    }
+    munmap(br, nentries * sizeof(struct io_uring_buf));
+    return 0;
 }
 #endif
