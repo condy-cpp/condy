10222e74d38002e6b77cd0dbeb0560de53b64daa
wokron <yi1tang.yang@gmail.com>
2026-08-06 15:23:15 +0800

    op_sender are standard senders

--- include/condy/senders.hpp
+++ include/condy/senders.hpp
@@ -6,11 +6,77 @@
 #pragma once
 
 #include "condy/detail/senders.hpp"
+#ifdef CONDY_HAS_EXECUTION
+#include "condy/detail/execution.hpp"
+#endif
 
 namespace condy {
 
-// TODO: This re-export is intentional. We may adapt these senders to standard
-// sender/receiver concepts in the future.
+#ifdef CONDY_HAS_EXECUTION
+
+template <PrepFuncLike PrepFunc, CQEHandlerLike CQEHandler>
+using OpSender = detail::StandardSender<detail::OpSender<PrepFunc, CQEHandler>>;
+
+template <PrepFuncLike PrepFunc, CQEHandlerLike CQEHandler,
+          typename MultiShotFunc>
+using MultiShotOpSender = detail::StandardSender<
+    detail::MultiShotOpSender<PrepFunc, CQEHandler, MultiShotFunc>>;
+
+template <PrepFuncLike PrepFunc, CQEHandlerLike CQEHandler, typename FreeFunc>
+using ZeroCopyOpSender = detail::StandardSender<
+    detail::ZeroCopyOpSender<PrepFunc, CQEHandler, FreeFunc>>;
+
+template <unsigned int Flags, typename Sender>
+using FlaggedOpSender =
+    detail::StandardSender<detail::FlaggedOpSender<Flags, Sender>>;
+
+template <typename... Senders>
+using ParallelAllSender =
+    detail::StandardSender<detail::ParallelAllSender<Senders...>>;
+
+template <typename... Senders>
+using ParallelAnySender =
+    detail::StandardSender<detail::ParallelAnySender<Senders...>>;
+
+template <typename... Senders>
+using WhenAllSender = detail::StandardSender<detail::WhenAllSender<Senders...>>;
+
+template <typename... Senders>
+using WhenAnySender = detail::StandardSender<detail::WhenAnySender<Senders...>>;
+
+template <typename... Senders>
+using LinkSender = detail::StandardSender<detail::LinkSender<Senders...>>;
+
+template <typename... Senders>
+using HardLinkSender =
+    detail::StandardSender<detail::HardLinkSender<Senders...>>;
+
+template <typename Sender>
+using RangedParallelAllSender =
+    detail::StandardSender<detail::RangedParallelAllSender<Sender>>;
+
+template <typename Sender>
+using RangedParallelAnySender =
+    detail::StandardSender<detail::RangedParallelAnySender<Sender>>;
+
+template <typename Sender>
+using RangedWhenAllSender =
+    detail::StandardSender<detail::RangedWhenAllSender<Sender>>;
+
+template <typename Sender>
+using RangedWhenAnySender =
+    detail::StandardSender<detail::RangedWhenAnySender<Sender>>;
+
+template <typename Sender>
+using RangedLinkSender =
+    detail::StandardSender<detail::RangedLinkSender<Sender>>;
+
+template <typename Sender>
+using RangedHardLinkSender =
+    detail::StandardSender<detail::RangedHardLinkSender<Sender>>;
+
+#else
+
 using detail::FlaggedOpSender;
 using detail::HardLinkSender;
 using detail::LinkSender;
@@ -27,5 +93,7 @@ using detail::RangedWhenAnySender;
 using detail::WhenAllSender;
 using detail::WhenAnySender;
 using detail::ZeroCopyOpSender;
+
+#endif
 
 } // namespace condy
